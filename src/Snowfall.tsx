400a4327b22b83bbd51f48ba20bbceb5d58b21f2
Keith Ingram <kwingram25@gmail.com>
2020-11-29 14:53:11 -0800

    defaults

--- src/Snowfall.tsx
+++ src/Snowfall.tsx
@@ -20,7 +20,7 @@ const Snowfall = ({ snowflakeCount = 150, style, ...config }: SnowfallProps = {}
   const animationFrame = useRef(0)
 
   const lastUpdate = useRef(Date.now())
-  const mergedConfig = useMemo(() => ({ ...defaultConfig, ...config }), [config])
+  const mergedConfig = { ...defaultConfig, ...config };
   const snowflakes = useSnowflakes(canvasRef, snowflakeCount, mergedConfig)
 
   const updateCanvasRef = (element: HTMLCanvasElement) => {
